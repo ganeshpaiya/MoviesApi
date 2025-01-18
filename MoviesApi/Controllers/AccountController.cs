de1ec4c82a74b2655b1e764627fc76a2d75ff20a
ganeshpaiya <ganesanhere4u@gmail.com>
2025-01-18 19:02:40 +0530

    Add project files.

---
+++ MoviesApi/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration configuration;
+        public AccountController(UserManager<AppUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            this.configuration = configuration;
+        }
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterNewUser(dtoNewUser dtouser)
+        {
+            if (ModelState.IsValid)
+            {
+                //map dto with appuser
+                AppUser appUser = new()
+                {
+                    UserName = dtouser.userName,
+                    Email = dtouser.email,
+                };
+                IdentityResult result = await _userManager.CreateAsync(appUser, dtouser.password);
+                if (result.Succeeded)
+                {
+                    return Ok("Success");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogIn(dtoLogin dtolog)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser? user = await _userManager.FindByNameAsync(dtolog.userName);
+                if (user != null)
+                {
+                    if (await _userManager.CheckPasswordAsync(user, dtolog.password))
+                    {
+                        var claims = new List<Claim>();
+                        //claims.Add(new Claim("name", "value"));
+                        claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                        claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id));
+                        claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+                        var roles = await _userManager.GetRolesAsync(user);
+                        foreach (var role in roles)
+                        {
+                            claims.Add(new Claim(ClaimTypes.Role, role.ToString()));
+                        }
+                        //signingCredentials
+                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]));
+                        var sc = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                        var token = new JwtSecurityToken(
+                            claims: claims,
+                            issuer: configuration["JWT:Issuer"],
+                            audience: configuration["JWT:Audience"],
+                            expires: DateTime.Now.AddHours(1),
+                            signingCredentials: sc
+                            );
+                        var _token = new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            expiration = token.ValidTo,
+                        };
+                        return Ok(_token);
+
+                    }
+                    else
+                    {
+                        return Unauthorized();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User Name is invalid");
+                }
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
